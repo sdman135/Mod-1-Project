fb40677ce2e71307f359ce6e173da8339be43a28
Dave Bletsch <davebletsch@gmail.com>
2019-10-29 14:51:44 -0400

    initially added daves files

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+user = "admin"
+password = "davebletsch"
+host = "flatiron-database-100719.cbrbgcquubb9.us-east-2.rds.amazonaws.com"
